71b48dd91e3723933eedc27c0957de3935c7323c
Ajesh Ajayan <ajeshajayan8893@gmail.com>
2021-01-19 20:57:22 +0530

    create manager user

--- api/managers/controllers/managers.js
+++ api/managers/controllers/managers.js
@@ -1,8 +1,69 @@
-'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+const { sendPasswordThruMail } = require('../../../common');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+    /**
+     * Create a record.
+     *
+     * @return {Object}
+     */
 
-module.exports = {};
+    async create(ctx) {
+
+        const createUser = async (data) => {
+            /**@type {String} */
+            const fullname = data.fullname;
+            const password = `${fullname.replace(' ').toLowerCase()}@avials${Math.floor(Math.random() * 1000)}`;
+            const userData = {
+                username: data.email,
+                email: data.email,
+                password: password
+            };
+            await sendPasswordThruMail(data.email, password);
+            const user = await strapi.query("user", "users-permissions").create(userData)
+            return user
+        };
+
+        let entity;
+        let userEntiry;
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            userEntiry = await createUser(data);
+            data.users_permissions_user = userEntiry;
+            entity = await strapi.services.managers.create(data, { files });
+        } else {
+            userEntiry = await createUser(ctx.request.body);
+            ctx.request.body.users_permissions_user = userEntiry;
+            entity = await strapi.services.managers.create(ctx.request.body);
+        }
+        return sanitizeEntity(entity, { model: strapi.models.managers });
+    },
+
+    async createUser(ctx) {
+
+        if (ctx.model == 'managers' && ctx.event === 'entry.create') {
+            /**@type {String} */
+            const fullname = data.fullname;
+            const password = `${fullname.replace(' ').toLowerCase()}@avials${Math.floor(Math.random() * 1000)}`;
+            const userData = {
+                username: data.email,
+                email: data.email,
+                password: password
+            };
+            await sendPasswordThruMail(data.email, password);
+            const user = await strapi.query("user", "users-permissions").create(userData)
+
+            const manager = await strapi.services.managers.update(
+                {
+                    id: ctx.entry.id
+                },
+                {
+                    ...ctx.entry,
+                    users_permissions_user: user
+                }
+            );
+
+            return manager;
+        }
+    }
+};
--- api/managers/models/managers.js
+++ api/managers/models/managers.js
@@ -1,8 +1,24 @@
 'use strict';
+const { sendPasswordThruMail } = require('../../../common');
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+            const fullname = data.fullname;
+            const password = `${fullname.replace(' ', '').toLowerCase()}@avials${Math.floor(Math.random() * 1000)}`;
+            const userData = {
+                username: data.email,
+                email: data.email,
+                password: password
+            };
+            await sendPasswordThruMail(data.email, password);
+            const user = await strapi.query("user", "users-permissions").create(userData)
+            data.users_permissions_user = user;
+        },
+    },
+};
