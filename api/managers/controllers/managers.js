2f76188539ba44200e68c7acd7a1de268e631714
Ajesh Ajayan <ajeshajayan8893@gmail.com>
2021-01-28 22:53:44 +0530

    oder status update

--- api/managers/controllers/managers.js
+++ api/managers/controllers/managers.js
@@ -24,5 +24,35 @@ module.exports = {
     async getByUserId(ctx) {
         const response = await strapi.services.managers.find({ users_permissions_user: ctx.params.userId });
         return sanitizeEntity(response, { model: strapi.models.managers })
+    },
+
+    async getManagerOrders(ctx) {
+        try {
+            let start = ctx.query._start;
+            let limit = ctx.query._limit;
+            let id = ctx.params.id;
+
+            if (!start || !limit) {
+                return ctx.send({
+                    message: '_limit and _start is expected'
+                }, 400);
+            }
+
+            /**@type {Array} */
+            let response = await strapi.services.orders.find({ manager: id , _start: start, _limit: limit }, [
+                'public_user',
+                'public_user.place',
+                'public_user',
+                'public_user.profile_pic',
+                'order_children.shop_user',
+                'order_children.shop_user.shop_name',
+                'order_children.public_user',
+
+            ]);
+
+            return sanitizeEntity(response, { model: strapi.models.orders });
+        } catch (e) {
+            console.log('error getManagerOrders', e);
+        }
     }
 };
